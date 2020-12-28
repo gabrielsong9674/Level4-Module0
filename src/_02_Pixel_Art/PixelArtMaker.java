7f76b1216a7bddbfe5fd1f28d4f465d2765dbcc5
gabrielsong9674 <gracegabrielson@icloud.com>
2020-12-27 19:10:37 -0600

    PixelArtSerializable

--- src/_02_Pixel_Art/PixelArtMaker.java
+++ src/_02_Pixel_Art/PixelArtMaker.java
@@ -4,20 +4,24 @@ import java.awt.FlowLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class PixelArtMaker implements MouseListener, Serializable{
+public class PixelArtMaker implements MouseListener{
 	private JFrame window;
 	private GridInputPanel gip;
 	private GridPanel gp;
 	ColorSelectionPanel csp;
 	private JButton saveButton;
+	private JButton loadButton;
+
 	private static final String DATA_FILE = "src/_02_Pixel_Art/saved.dat";
 
 	
@@ -36,13 +40,26 @@ public class PixelArtMaker implements MouseListener, Serializable{
 		gp = new GridPanel(w, h, r, c);
 		csp = new ColorSelectionPanel();
 		saveButton = new JButton("Save");
+		loadButton = new JButton("Load");
 		window.remove(gip);
 		window.add(gp);
 		window.add(csp);
 		window.add(saveButton);
+		window.add(loadButton);
+
 		saveButton.addActionListener((e)->{
 			try (FileOutputStream fos = new FileOutputStream(new File(DATA_FILE)); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-				oos.writeObject(gp);
+				oos.writeObject(gp.pixels);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		});
+		loadButton.addActionListener((e)->{
+			try (FileInputStream fis = new FileInputStream(new File(DATA_FILE)); ObjectInputStream ois = new ObjectInputStream(fis)) {
+				gp.pixels = (Pixel[][])ois.readObject();
+				gp.repaint();
+			} catch (ClassNotFoundException e1) {
+				e1.printStackTrace();
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
@@ -54,6 +71,7 @@ public class PixelArtMaker implements MouseListener, Serializable{
 	
 	public static void main(String[] args) {
 		new PixelArtMaker().start();
+		
 	}
 
 	@Override
